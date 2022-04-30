50c41f9f8a02acd0c6794d88232e555433b48db7
SemonoffArt <semonoff@gmail.com>
2022-04-30 18:49:01 +1100

    Overload in progres

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -7,6 +7,9 @@ void Print(int arr[], const int n);
 int Sum(int arr[], const int n);
 double Avg (int arr[], const int n);
 void ShiftLeft (int arr[], const int n, const int number_of_shifts);
+void ShiftRight (int arr[], const int n, const int number_of_shifts);
+int MaxValue(int arr[], const int n);
+int MinValue(int arr[], const int n);
 
 void main()
 {
@@ -17,20 +20,28 @@ void main()
 	Print(arr, n);
 	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
 	cout << "Среднее арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение в массиве: " << MinValue(arr, n) << endl;
+	cout << "Максимальное значение в массиве: " << MaxValue(arr, n) << endl;
 	int number_of_shifts;
 	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
 	ShiftLeft(arr, n, number_of_shifts);
 	Print(arr, n);
+	//------------------------------------------------
 	const int SIZE = 8;
 	int brr[SIZE];
 	FillRand(brr, SIZE);
 	Print(brr, SIZE);
 	cout << "Сумма элементов массива: " << Sum(brr, SIZE) << endl;
 	cout << "Среднее арифметическое элементов массива: " << Avg(brr, SIZE) << endl;
-
+	cout << "Минимальное значение в массиве: " << MinValue(brr, SIZE) << endl;
+	cout << "Максимальное значение в массиве: " << MaxValue(brr, SIZE) << endl;
 	//int number_of_shifts;
 	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
 	ShiftLeft(brr, SIZE, number_of_shifts);
+	Print(brr, SIZE);
+	//Mаксимальное значение элементов массива:
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	ShiftRight(brr, SIZE, number_of_shifts);
 	Print(brr, SIZE);
 }
 //Заполнение массива случайными числами:
@@ -48,9 +59,10 @@ void Print(int arr[], const int n)
 	{
 		cout<<arr[i]<<"\t";
 	}
+	cout << endl;
 }
 //Сумма элементов массива:
-int SUM(int arr[], const int n)
+int Sum(int arr[], const int n)
 {
 	int sum = 0;
 	for (int i = 0; i < n; i++)
@@ -64,7 +76,7 @@ double Avg(int arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
 }
-//Количество сдвигов элементов массива:
+//Количество сдвигов элементов массива влево:
 void ShiftLeft(int arr[], const int n, const int number_of_shifts)
 {
 	for (int i = 0; i < number_of_shifts; i++)
@@ -77,4 +89,44 @@ void ShiftLeft(int arr[], const int n, const int number_of_shifts)
 		arr[n - 1] = buffer;
 	}
 }
-a
+
+//Количество сдвигов элементов массива вправо:
+void ShiftRight(int arr[], const int n, const int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[n-1];
+		for (int i = n-1; i > 0; i--)
+		{
+			arr[i] = arr[i-1];
+		}
+		arr[0] = buffer;
+	}
+}
+// Максимальное значение элементов массива:
+int MaxValue(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; ++i)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	return  max;
+}
+
+	//Минимальное  значение элементов массива:
+	int MinValue(int arr[], const int n)
+	{
+		int min = arr[0];
+		for (int i = 0; i < n; ++i)
+	{
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+		return  min;
+}
